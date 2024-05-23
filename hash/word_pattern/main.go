36f2c3a1606be3628dd6d4e71224e5aaeadcc6c5
zhiyang.zhang <zhiyang.zhang@woqutech.com>
2024-05-23 19:54:57 +0800

    chore:完成290 单词规律

---
+++ hash/word_pattern/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"strings"
+)
+
+/*
+某座映射规律，那也做map1
+这题与 同构字符串 其实没有区别
+*/
+
+func wordPattern(pattern string, s string) bool {
+	p2s := make(map[byte]string)
+	s2p := make(map[string]byte)
+	// 拆分 s
+	s1 := strings.Split(s, " ")
+	if len(pattern) != len(s1) {
+		return false
+	}
+	for i := range pattern {
+		x, y := pattern[i], s1[i]
+		if _, ok := p2s[x]; !ok {
+			p2s[x] = y
+		}
+		if _, ok := s2p[y]; !ok {
+			s2p[y] = x
+		}
+		if p2s[x] != y || s2p[y] != x {
+			return false
+		}
+	}
+	return true
+}
