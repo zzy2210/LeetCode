b99dc9189afabf0135beab108d0700be6b5463b8
y1nhui <44086018+zzy2210@users.noreply.github.com>
2024-07-02 19:28:34 +0800

    Feature spiral matrix (#4)
    * Feat:螺旋矩阵
    
    * chore:更新
    
    * chore:
    
    ---------
    
    Co-authored-by: zhiyang.zhang <zhiyang.zhang@woqutech.com>

---
+++ matrix/life_game/go.mod
@@ -0,0 +1,3 @@
+module life_game
+
+go 1.21.1
---
+++ stack/valid_parentheses/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+func setZeroes(matrix [][]int) {
+	row := make([]bool, len(matrix))
+	col := make([]bool, len(matrix[0]))
+	for i, rows := range matrix {
+		for j, v := range rows {
+			if v == 0 {
+				row[i] = true
+				col[j] = true
+			}
+		}
+	}
+
+	for i, rows := range matrix {
+		for j := range rows {
+			if row[i] || col[j] {
+				rows[j] = 0
+			}
+		}
+	}
+}
