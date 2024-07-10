81405d04c6e81aaa68f823898eaf5420ad66f998
y1nhui <44086018+zzy2210@users.noreply.github.com>
2024-07-10 15:51:30 +0800

    Feature browser history (#7)
    * add:design browser history
    
    * chore:浏览器前进后退
    
    * chore:浏览器前进后退
    
    * chore:浏览器前进后退
    
    ---------
    
    Co-authored-by: zhiyang.zhang <zhiyang.zhang@woqutech.com>

---
+++ stack/design_browser_history/main.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+
+// 一个堆栈
+// 下一页就是入栈，上一页就是出栈
+// 返回 出栈的内容 同时放入另一个栈内
+
+// 这里有个问题，我自己推的结果和 leetcode给的结果不同
+// 是因为  visit 我理解错了吗
+
+func main() {
+	bro := Constructor("leetcode.com")
+	bro.Visit("google.com")
+	bro.Visit("facebook.com")
+	bro.Visit("youtube.com")
+	fmt.Println(bro.Back(1))
+	fmt.Println(bro.Back(1))
+	fmt.Println(bro.Forward(1))
+	bro.Visit("linkedin.com")
+	fmt.Println(bro.Forward(2))
+	fmt.Println(bro.Back(2))
+	fmt.Println(bro.Back(7))
+
+}
+
+type BrowserHistory struct {
+	preLst []string // 前进队列
+	curLst []string // 后退队列
+	url    string   // 当前
+}
+
+// 初始化浏览器
+func Constructor(homepage string) BrowserHistory {
+
+	return BrowserHistory{
+		preLst: []string{},
+		curLst: []string{},
+		url:    homepage,
+	}
+}
+
+// 跳转页面
+// 会清空前面的历史记录
+func (this *BrowserHistory) Visit(url string) {
+	this.curLst = append(this.curLst, this.url)
+	this.preLst = []string{}
+	this.url = url
+}
+
+func (this *BrowserHistory) Back(steps int) string {
+	if len(this.curLst) == 0 {
+		return this.url
+	}
+	if steps >= len(this.curLst) {
+		steps = len(this.curLst)
+	}
+
+	tmpList := this.curLst[len(this.curLst)-steps:]
+	this.curLst = this.curLst[:len(this.curLst)-steps]
+	// 反过来 不能正则拿
+	for i := range tmpList {
+		this.preLst = append(this.preLst, this.url)
+		this.url = tmpList[len(tmpList)-1-i]
+	}
+	return this.url
+}
+
+func (this *BrowserHistory) Forward(steps int) string {
+	if len(this.preLst) == 0 {
+		return this.url
+	}
+	if steps >= len(this.preLst) {
+		steps = len(this.preLst)
+	}
+	tmpList := this.preLst[len(this.preLst)-steps:]
+	this.preLst = this.preLst[:len(this.preLst)-steps]
+	for i := range tmpList {
+		this.curLst = append(this.curLst, this.url)
+		this.url = tmpList[len(tmpList)-1-i]
+	}
+	return this.url
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * obj := Constructor(homepage);
+ * obj.Visit(url);
+ * param_2 := obj.Back(steps);
+ * param_3 := obj.Forward(steps);
+ */
