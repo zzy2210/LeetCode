0406632bc88cc2b4f4096f1e545c3acdec04eac0
y1nhui <44086018+zzy2210@users.noreply.github.com>
2024-07-10 15:51:06 +0800

    add:group anagrams (#6)

--- hash/group_anagrams/main.go
+++ hash/group_anagrams/main.go
@@ -1,6 +1,22 @@
-pakcage main
+package main
 
-// TODO 待写
+import "sort"
+
+// map 的 k-v k是 某个单词排序后的结果，v放它本来的内容
+// 排完之后 直接把每个 v 给 append 进去就完事
+
 func groupAnagrams(strs []string) [][]string {
-
+	dict := make(map[string][]string)
+	for _, str := range strs {
+		b := []byte(str)
+		// 最大的难点居然是这个sort的使用，我真的很少用
+		sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+		key := string(b)
+		dict[key] = append(dict[key], str)
+	}
+	var result [][]string
+	for _, array := range dict {
+		result = append(result, array)
+	}
+	return result
 }
