81ca7a79fbb22317d41d1f79604666cd0bc83786
zhiyang.zhang <zhiyang.zhang@woqutech.com>
2024-07-10 20:03:18 +0800

    feature:包含数2

---
+++ hash/contains_duplicate_II/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_containsNearbyDuplicate(t *testing.T) {
+	cases := []struct {
+		nums   []int
+		k      int
+		result bool
+	}{
+		{[]int{1, 2, 3, 1}, 3, true},
+		{[]int{1, 0, 1, 1}, 1, true},
+		{[]int{1, 2, 3, 1, 2, 3}, 2, false},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, false},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3, false},
+	}
+	for _, c := range cases {
+		got := containsNearbyDuplicate(c.nums, c.k)
+		if got != c.result {
+			t.Errorf("nums: %v, k: %d, got: %v, expect: %v", c.nums, c.k, got, c.result)
+		}
+	}
+}
