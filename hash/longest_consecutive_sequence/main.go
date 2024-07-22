6a79f31340c9a44301b0777670cc24aeb48fd697
y1nhui <44086018+zzy2210@users.noreply.github.com>
2024-07-22 17:02:10 +0800

    chore (#9)
    Co-authored-by: zhiyang.zhang <zhiyang.zhang@woqutech.com>

---
+++ hash/longest_consecutive_sequence/main.go
@@ -0,0 +1,27 @@
+package main
+
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	numSet := map[int]bool{}
+	for _, v := range nums {
+		numSet[v] = true
+	}
+	result := 0
+	for num := range numSet {
+		if !numSet[num-1] {
+			current := num
+			cruuentStreak := 1
+			for numSet[current+1] {
+				current++
+				cruuentStreak++
+			}
+			if result < cruuentStreak {
+				result = cruuentStreak
+			}
+		}
+	}
+	return result
+
+}
